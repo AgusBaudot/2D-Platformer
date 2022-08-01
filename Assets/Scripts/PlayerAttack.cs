071e0bb7886a6c20e23546140d9838160e6f0b67
AgusBaudot <101721143+AgusBaudot@users.noreply.github.com>
2022-08-01 13:39:20 -0300

    Initial commit

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public Animator anim;
+    private Rigidbody2D body;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hola");
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector2.up * 2);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D body;
+    public float speed;
+    public float jumpPower;
+    Animator anim;
+    private BoxCollider2D boxCollider;
+    public LayerMask groundLayer;
+    public LayerMask wallLayer;
+    private float wallJumpCooldown;
+    private float horizontalImput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalImput = Input.GetAxis("Horizontal");
+
+        if (horizontalImput> 0.01f)
+        {
+            transform.localScale = Vector2.one;
+        } else if (horizontalImput< -0.01f)
+        {
+            transform.localScale = new Vector2(-1, 1);
+        }
+        anim.SetBool("run", horizontalImput != 0);
+        anim.SetBool("grounded", isGrounded());
+
+        if (wallJumpCooldown > 0.2f)
+        {
+            body.velocity = new Vector2(horizontalImput * speed, body.velocity.y);
+            if (onWall() && !isGrounded())
+            {
+                body.gravityScale = 0;
+                body.velocity = Vector2.zero;
+            }
+            else
+            {
+                body.gravityScale = 7;
+            }
+            if (Input.GetKey(KeyCode.Space))
+            {
+                Jump();
+            }
+        }
+        else
+        {
+            wallJumpCooldown += Time.deltaTime;
+        }
+    }
+
+    private void Jump()
+    {
+        if (isGrounded())
+        {
+            body.velocity = new Vector2(body.velocity.x, jumpPower);
+            anim.SetTrigger("jump");
+        } else if (onWall() && !isGrounded())
+        {
+            if(horizontalImput == 0)
+            {
+                body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 10, 0);
+                transform.localScale = new Vector3(-Mathf.Sign(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+            }
+            else
+            {
+                body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 3, 6);
+            }
+            wallJumpCooldown = 0;
+        }
+    }
+    private bool isGrounded()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        return raycastHit.collider != null;
+    }
+    private bool onWall()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, new Vector2 (transform.localScale.x, 0), 0.1f, wallLayer);
+        return raycastHit.collider != null;
+    }
+    private bool canAttack()
+    {
+        return horizontalImput == 0 && isGrounded() && !onWall();
+    }
+}
